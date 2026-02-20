0b82deea2e9d169dfe990e3c7664315669659431
ChinChin2k5 <dgtienchien14@gmail.com>
2026-02-21 06:38:04 +0700

    Viết thêm 1 chút về fluent API

---
+++ CoffeeShop.Infrastructure/Persistance/Contexts/Configurations/IngredientConfiguration.cs
@@ -0,0 +1,22 @@
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using MyCoffeeApp.Domain.Entities.Auth;
+
+namespace MyCoffeeApp.Infrastructure.Persistence.Contexts.Configurations
+{
+    public class IngredientConfiguration : IEntityTypeConfiguration<Ingredient>
+    {
+        public void Configure(EntityTypeBuilder<Ingredient> builder)
+        {
+            builder.ToTable("Ingredients");
+            builder.HasKey(e => e.UserId);
+            builder.Property(e => e.FullName).IsRequired().HasMaxLength(50);
+            builder.Property(e => e.Phone).IsRequired().HasMaxLength(20);
+            builder.Property(e => e.Avatar).IsRequired().HasMaxLength(500);
+            builder.HasOne(u => u.Users)
+                   .WithOne(u => u.UserProfile)
+                   .HasForeignKey<UserProfile>(up => up.UserId)
+                   .OnDelete(DeleteBehaviour.Cascade);
+        }
+    }
+}
