4ecdff5cdc0d8ca1e5dd59cb106d74195cd87268
ChinChin2k5 <dgtienchien14@gmail.com>
2026-02-21 12:41:44 +0700

    Set thêm mối quan hệ 1-n cho Category và Product

--- CoffeeShop.Domain/Entities/Catalog/Category.cs
+++ CoffeeShop.Domain/Entities/Catalog/Category.cs
@@ -7,6 +7,7 @@ namespace CoffeeShop.Domain.Entities.Catalog
         public string Name { get; set; }
         public string Description { get; set; }
         public int DisplayOrder { get; set; }
-        public bool IsActive {get; set; }
+        public bool IsActive { get; set; }
+        public virtual ICollection<Product> Products { get; set;}
     }
 }
--- CoffeeShop.Domain/Entities/Catalog/Product.cs
+++ CoffeeShop.Domain/Entities/Catalog/Product.cs
@@ -7,5 +7,6 @@ namespace CoffeeShop.Domain.Entities.Catalog
         public string Name { get; set; }
         public decimal Price { get; set; }
         public string Image { get; set; }
+        public virtual Category Category { get; set; }
     }
 }
